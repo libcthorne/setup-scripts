8abdc86ea26cae596028797972ae73c841345e33
Daniel Silver <das2c3@mail.missouri.edu>
2014-12-31 22:43:56 -0500

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -19,7 +19,6 @@ source ~/.rvm/scripts/rvm
 sudo apt-get install git
 
 # install chrome stable
-
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
