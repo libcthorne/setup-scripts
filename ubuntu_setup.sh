c844d73068119b590b2af2482dadc9b7ed6156bc
Daniel Silver <das2c3@mail.missouri.edu>
2015-01-03 13:42:51 -0500

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -59,3 +59,8 @@ sudo apt-get install virtualbox
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
+
+# install android studio
+sudo add-apt-repository ppa:paolorotolo/android-studio
+sudo apt-get update
+sudo apt-get install android-studio
