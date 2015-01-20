3e0b18d0c0633f74e8ca2c795ae5f9524c8cf420
Daniel Silver <das2c3@mail.missouri.edu>
2015-01-20 15:02:14 -0500

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,8 +1,6 @@
 # Set this file as executable and run it with:
-
-# cd /path/to/file
-# chmod +x filename.sh
-# sudo ./filename.sh
+# chmod +x ubuntu_setup.sh
+# sudo ./ubuntu_setup.sh
 
 # update package index
 sudo apt-get update
@@ -11,12 +9,16 @@ sudo apt-get update
 sudo apt-get install curl
 
 # install rvm with the latest ruby
- \curl -sSL https://get.rvm.io | bash -s stable --ruby
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
 echo "source $HOME/.rvm/scripts/rvm" >> ~/.bash_profile
 source ~/.rvm/scripts/rvm
 
 # install git
 sudo apt-get install git
+
+# configure git
+git config --global user.name "Dan Silver"
+git config --global user.email "dannysilver3@gmail.com"
 
 # install chrome stable
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -38,10 +40,6 @@ gem install rails
 # create projects directory
 mkdir ~/projects
 
-# Configure git
-git config --global user.name Dan Silver
-git config --global user.email dannysilver3@gmail.com
-
 # install virtualbox
 sudo apt-get install virtualbox
 
@@ -55,9 +53,5 @@ sudo add-apt-repository ppa:paolorotolo/android-studio
 sudo apt-get update
 sudo apt-get install android-studio
 
-# build android projects on 64bit ubuntu (http://stackoverflow.com/a/27734148)
-sudo apt-get install lib32stdc++6
-sudo apt-get install lib32z1
-
-# install oracle java jdk8 (auto accepts license)
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+#todo
+# install gparted
