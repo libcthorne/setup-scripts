007c7fc8d7d4133a1776d0aed8ee9861af9a019b
Daniel Silver <das2c3@mail.missouri.edu>
2016-02-14 22:01:07 -0800

    Create update-everything.sh

---
+++ update-everything.sh
@@ -0,0 +1,10 @@
+
+
+# Update all python packages
+# http://stackoverflow.com/a/3452888/2517012
+pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+
+
+# Update all node.js packages
+# https://docs.npmjs.com/getting-started/updating-global-packages
+npm update -g
